7a2be89ade5ad044a3e2a0732e7c06cfa9cee12a
梁志成 <Lzc_file@liangzhichengdemacbook-pro.local>
2016-08-22 14:26:58 +0800

    Initial Commit

---
+++ WYPractise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYPractise
+//
+//  Created by 梁志成 on 16/8/22.
+//  Copyright © 2016年 梁志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYPractise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYPractise
+//
+//  Created by 梁志成 on 16/8/22.
+//  Copyright © 2016年 梁志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
